47565a2042a3c899e60879d6400eb6782dbb8cc8
Guillaume Sabran <sabranguillaume@gmail.com>
2020-05-28 18:09:03 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Cabbage",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,6 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cabbage",
-            dependencies: ["AVFoundation", "CoreGraphics", "CoreMedia", "CoreImage", "Accelerate"]),
+            dependencies: []),
     ]
 )
